4a2d4242af9255d8bde91ad66b83f52ecf0a7fa3
ragavii45 <ragaviraja3472@gmail.com>
2026-08-11 14:14:16 +0530

    Create 4332-CheckGoodInteger

---
+++ 4332-CheckGoodInteger/4332-CheckGoodInteger.java
@@ -0,0 +1,14 @@
+// Last updated: 11/08/2026, 14:14:17
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int ds=0;
+        int ss=0;
+        while(n>0){
+            int d=n%10;
+            ds+=d;
+            ss+=d*d;
+            n/=10;
+        }
+        return(ss-ds)>=50;
+    }
+}
