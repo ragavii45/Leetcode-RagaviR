8702313c9218b540cd73bbb19d1798ef45288ec4
ragavii45 <ragaviraja3472@gmail.com>
2026-08-11 14:19:54 +0530

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,11 @@
+-- Last updated: 11/08/2026, 14:19:55
+# Write your MySQL query statement below
+SELECT
+    CASE
+        WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
+        WHEN id % 2 = 1 THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
