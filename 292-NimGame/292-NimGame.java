a6041f8cd4a839f0a50a26d64d1d51a48be51ee0
ragavii45 <ragaviraja3472@gmail.com>
2026-08-11 14:21:52 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 11/08/2026, 14:21:53
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4!=0)
+        return true;
+        else
+        return false;
+    }
+}
