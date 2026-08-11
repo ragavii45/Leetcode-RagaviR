5750beb6ef30f5009bae947e1d99b5db7ed6c906
ragavii45 <ragaviraja3472@gmail.com>
2026-08-11 14:21:49 +0530

    Create 316-RemoveDuplicateLetters

---
+++ 316-RemoveDuplicateLetters/316-RemoveDuplicateLetters.java
@@ -0,0 +1,37 @@
+// Last updated: 11/08/2026, 14:21:49
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] lastIndex = new int[26];
+        boolean[] seen = new boolean[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (seen[c - 'a']) {
+                continue;
+            }
+
+            while (!stack.isEmpty()
+                    && stack.peek() > c
+                    && lastIndex[stack.peek() - 'a'] > i) {
+                seen[stack.pop() - 'a'] = false;
+            }
+
+            stack.push(c);
+            seen[c - 'a'] = true;
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (char c : stack) {
+            ans.append(c);
+        }
+
+        return ans.toString();
+    }
+}
