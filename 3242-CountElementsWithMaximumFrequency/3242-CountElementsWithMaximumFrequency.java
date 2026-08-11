7168591158f9b16ac056006a94a48e9a1c8579f0
ragavii45 <ragaviraja3472@gmail.com>
2026-08-11 14:14:44 +0530

    Create 3242-CountElementsWithMaximumFrequency

---
+++ 3242-CountElementsWithMaximumFrequency/3242-CountElementsWithMaximumFrequency.java
@@ -0,0 +1,20 @@
+// Last updated: 11/08/2026, 14:14:45
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int maxFreq = 0;
+        for (int freq : map.values()) {
+            maxFreq = Math.max(maxFreq, freq);
+        }
+        int total = 0;
+        for (int freq : map.values()) {
+            if (freq == maxFreq) {
+                total += freq;
+            }
+        }
+        return total;
+    }
+}
