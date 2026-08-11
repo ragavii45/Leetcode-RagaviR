8471ebea4151cede74e38d4bae2d62c5103b3ba7
ragavii45 <ragaviraja3472@gmail.com>
2026-08-11 14:15:26 +0530

    Create 2715-KItemsWithTheMaximumSum

---
+++ 2715-KItemsWithTheMaximumSum/2715-KItemsWithTheMaximumSum.java
@@ -0,0 +1,10 @@
+// Last updated: 11/08/2026, 14:15:26
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if (k <= numOnes)
+         return k; 
+         if (k <= numOnes + numZeros) 
+         return numOnes; 
+         return numOnes - (k - numOnes - numZeros);
+    }
+}
