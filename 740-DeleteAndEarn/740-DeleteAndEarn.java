2ea0f106a060aa27068997746506cb4f47c5f777
ragavii45 <ragaviraja3472@gmail.com>
2026-08-11 14:19:23 +0530

    Create 740-DeleteAndEarn

---
+++ 740-DeleteAndEarn/740-DeleteAndEarn.java
@@ -0,0 +1,28 @@
+// Last updated: 11/08/2026, 14:19:23
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int max = 0;
+
+        // Find maximum element
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+
+        // Store total points for each number
+        int[] points = new int[max + 1];
+        for (int num : nums) {
+            points[num] += num;
+        }
+
+        // DP array
+        int[] dp = new int[max + 1];
+        dp[0] = 0;
+        dp[1] = points[1];
+
+        for (int i = 2; i <= max; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + points[i]);
+        }
+
+        return dp[max];
+    }
+}
