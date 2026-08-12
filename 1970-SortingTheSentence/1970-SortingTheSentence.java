af9a4645da63d53dc50daef62e479c2e69b09d5d
ragavii45 <ragaviraja3472@gmail.com>
2026-08-12 21:29:25 +0530

    Create 1970-SortingTheSentence

---
+++ 1970-SortingTheSentence/1970-SortingTheSentence.java
@@ -0,0 +1,19 @@
+// Last updated: 12/08/2026, 21:29:26
+class Solution {
+    public String sortSentence(String s) {
+
+        String[] words = s.split(" ");
+
+        String[] result = new String[words.length];
+
+        for (String word : words) {
+
+            int index = word.charAt(word.length() - 1) - '1';
+
+            result[index] =
+                word.substring(0, word.length() - 1);
+        }
+
+        return String.join(" ", result);
+    }
+}
