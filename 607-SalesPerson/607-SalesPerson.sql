6c5927c7eedae992add4469d3ebc2d80d32a8e3e
ragavii45 <ragaviraja3472@gmail.com>
2026-08-12 21:32:30 +0530

    Create 607-SalesPerson

---
+++ 607-SalesPerson/607-SalesPerson.sql
@@ -0,0 +1,11 @@
+-- Last updated: 12/08/2026, 21:32:30
+# Write your MySQL query statement below
+SELECT s.name
+FROM SalesPerson s
+WHERE s.sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders o
+    JOIN Company c
+        ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
