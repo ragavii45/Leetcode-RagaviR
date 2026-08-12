c2bf39e410647729830911191f9e29aa065abd5d
ragavii45 <ragaviraja3472@gmail.com>
2026-08-12 21:34:08 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.java
@@ -0,0 +1,15 @@
+// Last updated: 12/08/2026, 21:34:08
+class Solution {
+    public int[] countBits(int n) {
+
+        int[] answer = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+
+            answer[i] =
+                answer[i / 2] + (i % 2);
+        }
+
+        return answer;
+    }
+}
