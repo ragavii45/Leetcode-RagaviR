7ca2e214ad442f56b3b8dd770d47d8a196a8764a
ragavii45 <ragaviraja3472@gmail.com>
2026-08-12 21:33:14 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,29 @@
+// Last updated: 12/08/2026, 21:33:15
+import java.util.*;
+
+class Solution {
+
+    public int findContentChildren(
+        int[] g,
+        int[] s
+    ) {
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int child = 0;
+        int cookie = 0;
+
+        while (child < g.length &&
+               cookie < s.length) {
+
+            if (s[cookie] >= g[child]) {
+                child++;
+            }
+
+            cookie++;
+        }
+
+        return child;
+    }
+}
